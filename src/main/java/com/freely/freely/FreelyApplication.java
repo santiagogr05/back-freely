6500c6143ac1385e8ecd6ad1740388744f3367e2
SGR <san7iagogr.05@gmail.com>
2025-09-25 19:58:45 -0500

    Create project

---
+++ src/main/java/com/freely/freely/FreelyApplication.java
@@ -0,0 +1,13 @@
+package com.freely.freely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freely/freely/FreelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freely.freely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
