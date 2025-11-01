988905ee25f32e04f873331819e857abe5aa9b3c
SamAl404 <samualvarezh@gmail.com>
2025-11-01 00:42:39 -0500

    feat: add fields to the User entity class, with it's jpa decorators

--- src/main/java/com/freely/freely/entities/User.java
+++ src/main/java/com/freely/freely/entities/User.java
@@ -1,4 +1,20 @@
 package com.freely.freely.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private  String name;
+    @Column(unique = true)
+    private  String email;
+    private  String password;
+    private  String role;
+    private  String registrationDate;
 }
