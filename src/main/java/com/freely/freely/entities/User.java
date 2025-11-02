21ecd9759023418bb44c3be706e76180cbedd802
SamAl404 <samualvarezh@gmail.com>
2025-11-01 22:07:49 -0500

    update: make the entity class implements UserDetails to manage jwt workflow

--- src/main/java/com/freely/freely/entities/User.java
+++ src/main/java/com/freely/freely/entities/User.java
@@ -2,11 +2,19 @@ package com.freely.freely.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -15,6 +23,38 @@ public class User {
     @Column(unique = true)
     private  String email;
     private  String password;
-    private  String role;
-    private  String registrationDate;
+    @Enumerated(EnumType.STRING)
+    private  UserRole role;
+    private LocalDateTime registrationDate;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
