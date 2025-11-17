b75a85edcd81cc52c296145024c4c611f82906ba
SamAl404 <samualvarezh@gmail.com>
2025-11-16 22:15:00 -0500

    feat: initial controller for the freelancers

---
+++ src/main/java/com/freely/freely/controllers/FreelancerController.java
@@ -0,0 +1,25 @@
+package com.freely.freely.controllers;
+
+import com.freely.freely.DTO.freelancer.FreelancerCardDTO;
+import com.freely.freely.services.FreelancerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/freelancers")
+public class FreelancerController {
+
+    @Autowired
+    private FreelancerService service;
+
+    @GetMapping("/cards")
+    public ResponseEntity<List<FreelancerCardDTO>> getTopFreelancers() {
+        return ResponseEntity.ok(service.getTopFreelancers());
+    }
+
+}
